102f71c85b0ab5ffd4db9b6b9900a0d15d7345dd
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-23 19:59:15 +0530

    firebase

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// firebase
+
+
+// Import the functions you need from the SDKs you need
+import { } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js"
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjuhQChs-DPtRcczjC_HOfBVM-4woFYWw",
+  authDomain: "first-project-4a546.firebaseapp.com",
+  projectId: "first-project-4a546",
+  storageBucket: "first-project-4a546.appspot.com",
+  messagingSenderId: "434816906916",
+  appId: "1:434816906916:web:b19d5db2d2f287aeddd831"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const googlebtn = document.getElementById("sign-up-btn").addEventListener
+
+googlebtn.addEventListener("click", function () {
+  console.log("clicked");
+});
+
+
